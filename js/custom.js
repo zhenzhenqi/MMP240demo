cbff78fa36c107500fbeb2d974e9fdff902d1ac3
jane363636 <jane363636@gmail.com>
2015-03-23 12:00:18 -0400

    animation effect for landing page

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("nav").addClass('animated fadeIn');
-    
+    $("#featured_1").addClass('animated fadeInRight');
 });
 
 
@@ -8,16 +8,21 @@ $(document).ready(function () {
 
 $(window).scroll(function(){
 //    console.log("scrolled!");   
-    
     var how_much_document_scrolled = $(document).scrollTop();
+//    console.log(how_much_document_scrolled);
     
     var h1_offset_top = $('#tagline h1').offset().top;
+     console.log(h1_offset_top);
     
-    
-//    console.log(how_much_document_scrolled + "<," + h1_offset_top);
+//    console.log(how_much_document_scrolled + "," + h1_offset_top);
     
     if(h1_offset_top - how_much_document_scrolled < window.innerHeight){
         $('#tagline h1').addClass('animated fadeInDown');
     }
     
+    
+    var h3_offset_top = $('#founder h3').offset().top;
+    if(h3_offset_top - how_much_document_scrolled < window.innerHeight-100){
+        $('#founder h3').addClass('animated lightSpeedIn');
+    }
 });
